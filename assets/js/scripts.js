13f6b97c14946789f49bfc1045502025768bc5d2
franciscogo <frandresgo@gmail.com>
2019-03-17 22:28:39 -0400

    Chuck strikes back!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,75 +1,68 @@
-// https://api.chucknorris.io/
-
-// Usage
-// Retrieve a random chuck joke in JSON format.
-// https://api.chucknorris.io/jokes/random
-
-// Example response:
-// {
-//  "icon_url" : "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-//  "id" : "2B0-YIQIQDaX1jpxmLQCpA",
-//  "url" : "https://api.chucknorris.io/jokes/2B0-YIQIQDaX1jpxmLQCpA"
-//  "value" : "Chuck Norris isn't given tests. He TAKES them."
-// }
-// Get me a new one ...(press "r" to refresh)
-
-// Retrieve a random chuck norris joke from a given category.
-// https://api.chucknorris.io/jokes/random?category={category}
-
-// Retrieve a list of available categories.
-// https://api.chucknorris.io/jokes/categories
-
-// Free text search.
-// https://api.chucknorris.io/jokes/search?query={query}
-
-const jokesEndpoints = {
+const endpoints = {
   getRandom: 'https://api.chucknorris.io/jokes/random',
   getCategories: 'https://api.chucknorris.io/jokes/categories',
-  getByCategory: (category) => `https://api.chucknorris.io/jokes/random?category=${category}`
+  getByCategory: category => `https://api.chucknorris.io/jokes/random?category=${category}`
 }
 
-const joke = document.getElementById('joke');
-const categories = document.getElementById('categories');
+const categoriesContainer = document.querySelector('.categories');
+const jokeContainer = document.querySelector('.joke');
 
-const writeJoke = (value) => {
-  joke.textContent = value;
+async function htmlJoke (joke) {
+  jokeContainer.textContent = joke.value;
 }
 
-const addClickToChild = (categories) => {
-  categories.addEventListener('change', (event) => {
-    getJokeByCategory(event.target.value);
+async function handleClass (event) {
+  event.preventDefault();
+  document.querySelectorAll('.categories > li').forEach((category) => {
+    if (category.classList.contains('selected')) {
+      category.classList.remove('selected');
+    };
   })
-}
+  event.target.classList.add('selected');
+};
 
-const renderCategories = (array) => {
-  array.map((item) => {
-    let option = document.createElement('option');
-    option.value = item;
-    option.textContent = item;
-    categories.appendChild(option);
-  })
-  
-  addClickToChild(categories);
-}
-
-async function getRandomJoke () {
-  await fetch(jokesEndpoints.getRandom)
+async function handleJokeByCategory (category) {
+  return fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
     .then(response => response.json())
-    .then(response => writeJoke(response.value));
+    .then(response => htmlJoke(response));
 }
 
-getRandomJoke();
+async function handleCategory (event) {
+  handleJokeByCategory(event.target.textContent);
+}
 
-async function getCategories () {
-  await fetch(jokesEndpoints.getCategories)
+async function handleClick (event) {
+  event.preventDefault();
+  handleClass(event);
+  handleCategory(event);
+};
+
+async function htmlCategories (categories) {
+  categories.forEach(category => {
+    const listItem = document.createElement('li');
+    listItem.textContent = category;
+    listItem.onclick = handleClick;
+    categoriesContainer.appendChild(listItem);
+  });
+}
+
+async function categories () {
+  return fetch('https://api.chucknorris.io/jokes/categories')
     .then(response => response.json())
-    .then(response => renderCategories(response));
+    .then(response => htmlCategories(response))
+    .catch(error => console.log(error));
 }
 
-getCategories();
-
-async function getJokeByCategory (category) {
-  await fetch(jokesEndpoints.getByCategory(category))
+async function joke () {
+  return fetch('https://api.chucknorris.io/jokes/random')
     .then(response => response.json())
-    .then(response => writeJoke(response.value));
+    .then(response => htmlJoke(response))
+    .catch(error => console.log(error));
 }
+
+async function run () {
+  joke();
+  categories();
+}
+
+run();
